bc8191065b106c0fb009ee4ecf5ba1251264661b
VitorKCampanholi <adegawhc@outlook.com>
2024-11-20 01:14:22 -0300

    Adicionar arquivos de projeto.

---
+++ Vetores2/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vetores2
+{
+    internal class Product
+    {
+        public string Nome { get; set; }
+        public double Preço { get; set; }
+
+    }
+}
---
+++ Vetores2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Globalization;
+using Vetores2;
+
+
+int n = int.Parse(Console.ReadLine());
+
+Product[] vect = new Product[n];
+
+
+for (int i = 0; i < n; i++)
+{
+    string nome = Console.ReadLine();
+    double preço = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+    vect[i] = new Product { Nome = nome, Preço = preço };
+}
+
+double soma = 0.0;
+for (int i = 0; i < n; i++)
+{
+    soma += vect[i].Preço;
+
+}
+
+
+double avg = soma / n;
+Console.WriteLine("AVERAGE PRICE = R$ " + avg.ToString("F2"), CultureInfo.InvariantCulture);
